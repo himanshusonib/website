078ccd4dac392bab0242da98f7cba23a5e3a7b56
himanshu47545 <SONIHIMANSHU475@GMAIL.COM>
2025-03-08 20:55:55 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Toggle mobile menu
-const menuToggle = document.getElementById('menuToggle');
-const navMenu = document.getElementById('navMenu');
-
-menuToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-});
