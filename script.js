6c37f37ede32c6cb6998acd196fb836444c8b604
himanshu47545 <SONIHIMANSHU475@GMAIL.COM>
2025-03-08 17:28:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle mobile menu
+const menuToggle = document.getElementById('menuToggle');
+const navMenu = document.getElementById('navMenu');
+
+menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
